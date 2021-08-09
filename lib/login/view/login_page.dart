46798801524f85e2da43499ce454b19587324371
Emmanuel Chargoy <charger513@hotmail.com>
2021-08-09 13:26:39 -0500

    Finalizado

---
+++ lib/login/view/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login_bloc/login/login.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => LoginPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: BlocProvider(
+          create: (context) => LoginBloc(
+            authenticationRepository:
+                RepositoryProvider.of<AuthenticationRepository>(context),
+          ),
+          child: LoginForm(),
+        ),
+      ),
+    );
+  }
+}
